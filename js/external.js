f27e7b0b59cd425c8eb77031c7fb7740c6a1b534
Laura Davis <laura.l.davis.210@gmail.com>
2020-04-20 11:18:29 -0500

    Update after exercise walkthrough

--- js/external.js
+++ js/external.js
@@ -25,13 +25,13 @@ alert("Great, " + favoriteColor + " is my favorite color too!");
 // 3a. You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it),
 // and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much
 // will you have to pay?
-var dailyRateDollars = Number(prompt("Enter the price in dollars for a movie (per day):"));
-var littleMermaidDays = Number(prompt("Enter #days rental for The Little Mermaid:"));
-var brotherBearDays = Number(prompt("Enter #days rental for Brother Bear:"));
-var herculesDays = Number(prompt("Enter #days rental for Hercules:"));
+var dailyRateDollars = Number(prompt("Enter the daily movie rental price in dollars:"));
+var littleMermaidDays = Number(prompt("How many days rental for The Little Mermaid?:"));
+var brotherBearDays = Number(prompt("How many days rental for Brother Bear?:"));
+var herculesDays = Number(prompt("How many days rental for Hercules:"));
 var totalDays = littleMermaidDays + brotherBearDays + herculesDays;
 var totalPaymentDue = totalDays * dailyRateDollars;
-alert("3a. The total movie rental payment due is: $" + totalPaymentDue);
+alert("3a. The total movie rental payment due will be: $" + totalPaymentDue.toFixed(2));
 
 // 3b. Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different
 // rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week?
@@ -47,20 +47,22 @@ var amazonWeeklyPayDollars = amazonDollarsPerHr * amazonHrsWorked;
 var facebookWeeklyPayDollars = facebookDollarsPerHr * facebookHrsWorked;
 var totalWeeklyPayDollars = googleWeeklyPayDollars + amazonWeeklyPayDollars + facebookWeeklyPayDollars;
 
-alert("3b. Your total paycheck for this week is: $" + totalWeeklyPayDollars);
+alert("3b. Your total paycheck for this week is: $" + totalWeeklyPayDollars.toFixed(2));
 
 // 3c. A student can be enrolled in a class only if the class is not full and the class schedule does not conflict
 // with her current schedule.
-var seatsAvailable = true;
-var noScheduleConflict = true;
+var seatsAvailable = confirm("Confirm that the class is not full"); // boolean
+var noScheduleConflict = confirm("Confirm that the class schedule does not conflict"); // boolean
 var canEnroll = (seatsAvailable && noScheduleConflict);
-alert("3c. This student can enroll?: " + canEnroll);
+alert("3c. This student can enroll: " + canEnroll);
 
 // 3d. A product offer can be applied only if a person buys more than 2 items, and the offer has not expired.
 // Premium members do not need to buy a specific amount of products.
+var minItemsForDiscount = 2;
+var isProductOfferCurrent = true;
 var itemsPurchasedCount = Number(prompt("Enter the number of items you are purchasing:"));
-var isPremiumMember = false, isProductOfferCurrent = true;
-var isProductDiscountValid = isProductOfferCurrent && (itemsPurchasedCount > 2 || isPremiumMember);
+var isPremiumMember = confirm("Confirm (click OK) if you are a Premium Member. Cancel if you are not."); // boolean
+var isProductDiscountValid = isProductOfferCurrent && (itemsPurchasedCount > minItemsForDiscount || isPremiumMember);
 
-alert("3d. This product offer validation is: " + isProductDiscountValid);
+alert("3d. Is the product offer valid for this purchase?: " + isProductDiscountValid);
 
