6049b16f29b523bd9e5d015fec728268f38016fd
Laura Davis <laura.l.davis.210@gmail.com>
2020-05-12 11:20:43 -0500

    add lecture file

---
+++ js/jquery-selectors-lec.js
@@ -0,0 +1,102 @@
+"use strict";
+
+$(document).ready(function() {
+	//jQuery Docs ==> https://api.jquery.com/
+
+	/**********************************************
+	 * 		** General Selector Syntax **
+	 *********************************************/
+	// $('selector')
+
+
+
+
+	/**********************************************
+	 * 			** jQuery Methods **
+	 *********************************************/
+
+	// .html: returns the HTML contents of selected element or the first element in a collection. Similar to the innerHTML property we previously covered.
+	// .css: allows us to change CSS properties for a given element or elements. Similar to the style property previously discussed.
+
+
+	/**********************************************
+	 * 			** ID Selector **
+	 *********************************************/
+	// $('#some-id');
+
+	// TODO TOGETHER: Select the contents/html of the id 'book-welcome' and set equal to the variable 'contents'. Console.log the contents
+
+
+	// TODO TOGETHER: Change the background color of the id 'book-welcome'
+
+
+	// TODO: Select the contents/html of the id 'book-bundle-one' and set equal to the variable 'bookBundle'. Console.log the contents
+
+
+	// TODO: Change one css property of the id 'container'
+
+
+
+	/**********************************************
+	 * 			** CLASS Selector **
+	 *********************************************/
+	// $('.some-class');
+
+	// TODO TOGETHER: Change the font size of all the .main-headings
+
+
+	// TODO: Explore the following JS Bin example:
+	//  https://jsbin.com/topupe/1/edit?js,output
+
+
+	// TODO BONUS: Add additional class names to the page, and practice changing the css or viewing the contents
+
+
+
+	/**********************************************
+	 * 			** ELEMENT Selector **
+	 *********************************************/
+	//	$('tag_name')
+
+	// TODO TOGETHER: Change the background color of all paragraph tags
+
+
+	// TODO: Explore the following JS Bin example:
+	//  https://jsbin.com/gayir/1/edit?js,output
+
+
+	// TODO BONUS: Add additional elements to the page, and practice changing the css or viewing the contents
+
+
+	/**********************************************
+	 * 			** MULTIPLE Selector **
+	 *********************************************/
+	// $("selector1, selector2, ...")
+
+
+	// TODO TOGETHER: Change the background color of id 'mystery-genre' and 'book-bundle-three' to red
+
+
+
+	// TODO: Explore the following JS Bin example:
+	//  https://jsbin.com/qejeli/1/edit?output
+
+
+	// TODO BONUS: Add additional elements to the page with ids and classes, and practice changing the css or viewing the contents
+
+
+	/**********************************************
+	 * 			** THE ALL Selector **
+	 *********************************************/
+	// $('*')
+	// A common use is to put a border around every element to help see the layout of a page.
+
+	// TODO TOGETHER: Create a border around all elements
+
+
+
+	// TODO: Explore the following JS Bin example:
+	//  https://jsbin.com/fivucu/1/edit?js,output
+
+
+});
