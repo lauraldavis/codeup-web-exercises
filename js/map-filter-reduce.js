736d60ef61fce75489207b451d84bccd6079065d
Laura Davis <laura.l.davis.210@gmail.com>
2020-05-22 11:30:36 -0500

    Map, Filter, Reduce exercise - add walkthrough notes

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -57,37 +57,64 @@
 
     // 4. Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result
     // to calculate the average.
-    const sumYrs = users.reduce((sum, usr) => sum += usr.yearsOfExperience, 0);
-    const avgYrs = sumYrs / users.length;
-    console.log(`Total years of experience is: ${sumYrs}.\nAverage years of experience is: ${avgYrs}.`);
+    let sumYrsExp = users.reduce((sum, usr) => sum += usr.yearsOfExperience, 0);
+    let avgYrsExp = sumYrsExp / users.length;
+    console.log(`Total years of experience is: ${sumYrsExp}.\nAverage years of experience is: ${avgYrsExp}.`);
 
 
     // 5. Use .reduce to get the longest email from the list of users.
     // var bestSalesPerson =  salesPeople.reduce((bestSalesPerson, person) => (bestSalesPerson.units > person.units) ? bestSalesPerson : person, {units: 0});
-    const longestEmail = users.reduce((longestEmail, usr) => (longestEmail.email.length > usr.email.length) ? longestEmail : usr, {email: 0});
+    let longestEmail = users.reduce((longestEmail, usr) => (longestEmail.email.length > usr.email.length) ? longestEmail : usr, {email: 0});
+    // starting value example shown was empty string, but is not required because you're not accumulating but comparing values.
     console.log(longestEmail.email);
 
 
     // 6. Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
-    const userName = users.reduce((userName, user) => userName + `${user.name} `, "");
-    console.log(userName);
+    let userName = users.reduce((userName, user, index) => {
+        if(index < users.length - 1) {
+            return userName + `${user.name}, `
+        } else {
+            return userName + `${user.name}.`
+        }
+    }, "");
+    console.log(`Your instructors are: ${userName}`);
+
 
     // Bonus
 
     // 1. Use .reduce to get the unique list of languages from the list of users.
-    const userLang = users.reduce((userLang, user) => userLang + `${user.languages},`, "");
+    let userLang = users.reduce((userLang, user) => userLang + `${user.languages},`, "");
     // remove trailing comma and convert string to array
     const userLangArr = userLang.substring(0, userLang.length - 1).split(",").sort();
     // filter out unique values and convert to comma-separated string
-    const uniqLangStr = userLangArr.filter((element, index, array) => userLangArr.indexOf(element) === index).join(",");
+    let uniqLangStr = userLangArr.filter((element, index, array) => userLangArr.indexOf(element) === index);
+    // Add on join if you want to return it as a string: .join(",");
     console.log(uniqLangStr);
 
-    // refactor to make more readable and eliminate extra variables/steps
-    const uniqueLanguages = users
-        .reduce((uniqueLanguages, user) => uniqueLanguages + `${user.languages},`, "")
-        .substring(0, length - 1).split(",").sort()
-        .filter((language, index) => .indexOf(language) === index).join(","); // indexOf is not defined
-    console.log(uniqueLanguages);
+
+
+    // // David's solution
+    // const uniqueLangs = users
+    //     .reduce((langList, user) => {
+    //         for (let lang of user.languages) {
+    //             if(!langList.includes(lang)) {
+    //                 langList.push(lang);
+    //             }
+    //         }
+    //         return langList;
+    //     }, []);
+    // console.log(uniqueLangs);
+    //
+    // // another solution using Set (we will cover later)
+    // let arr = users.reduce((langs ,user) => {
+    //     for (let lang of user.languages) {
+    //         langs.push(lang);
+    //     }
+    //     return langs;
+    // }, []);
+    // let set = new Set(arr);
+    // let uniqueLangsQuick = Array.from(set);
+    // console.log(uniqueLangsQuick);
 
 
 })();
