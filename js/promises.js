b372ed799738ce34a2d03bc6ce779c044dbb6731
Laura Davis <laura.l.davis.210@gmail.com>
2020-05-26 12:59:52 -0500

    JS II - Promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,5 +1,4 @@
 "use strict";
-(function () {
 
     // 1. Create js/promises.js and link it in an html file
     // 2. Generate a GitHub API personal access token (add to keys.js)
@@ -11,51 +10,51 @@
     // looking for most recent push "event", look at the type property, use filter(), new Date(), .toDateString()
     // there is a type property (push event) for each event, in order by most recent event, filter out specific events by the date (timestamp)
 
-    function gitHubLastPush(username = 'lauraldavis') {
+    const getLastPush = (username = 'lauraldavis') => {
 
-        let gitHubAPIUsers = fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${GITHUB_KEY}`}});
-        // console.log(gitHubAPIUsers);
+        const gitHubAPIUsers = fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${GITHUB_KEY}`}});
 
-        gitHubAPIUsers
+        return gitHubAPIUsers
             .then(response => response.json())
-            // all events
+            // gets all events:
             // .then(events => console.log(new Date(events[0].created_at).toDateString()))
-            // just the push events
-            .then(events => console.log(new Date(events.filter(user => user.type === "PushEvent")[0].created_at).toDateString()))
 
-            // This doesn't work... need to figure out how to return the date value rather than log it
-            // .then(events => new Date(events.filter(user => user.type === "PushEvent")[0].created_at).toDateString())
+            // gets just the push events (console logs the date)
+            // .then(events => console.log(new Date(events.filter(user => user.type === "PushEvent")[0].created_at).toDateString()))
+
+            // return the date string rather than log it
+            .then(events => new Date(events.filter(user => user.type === "PushEvent")[0].created_at).toDateString())
+
             .catch(error => console.log(error));
-    }
-    gitHubLastPush();
+    };
+    getLastPush().then(console.log);
 
 
-    // test other users
-    // gitHubLastPush('jreich5');
-    // gitHubLastPush('Shelbypol')
+    // test w/ other users
+    // getLastPush('jreich5').then(console.log);
+
 
     // Create Your Own Promise
     // 1. Write a function named wait that accepts a number as a param and returns a promise that resolves after the passed #milliseconds
     // Bonus
     // 1. Make sure the promise resolves with the milliseconds in the return, so you can make the console log message more dynamic
 
-    function wait(num) {
-        return new Promise((resolve, reject) => {
+    const wait = num => {
+        return new Promise((resolve) => {
             setTimeout(() => {
-                if (num <= 3000) {
-                    resolve('Here is your data: ...');
-                    return console.log(`You'll see this after ${num / 1000} second(s)`);
+                if (typeof num === "number") {
+                    let secondPlural = (num <= 1000) ? "second" : "seconds";
+                    return console.log(`You'll see this after ${num / 1000} ${secondPlural}`);
                 } else {
-                    reject('Network Connection Error!');
-                    return console.log(`No response returned within ${num / 1000} second(s)`);
+                    return console.log(`${num} is not valid, please enter a number`)
                 }
             }, num);
         });
-    }
+    };
 
     wait(1000);
     wait(3000);
-    wait(5000);
+    wait(5500);
+    wait("test-non-numeric");
 
 
-})();
